ff6f1c059393c14f7703c6bced7f5f23d835a0d6
Pedro Muñoz <pedroanibal_jr@hotmail.com>
2023-06-24 21:13:13 -0500

    Tomar lista App

---
+++ tomaListaApp/HomeController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeController.swift
+//  tomaListaApp
+//
+//  Created by PEDRO MUNOZ BURGOS on 6/23/23.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tomaListaApp/Views/AuthHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthHeaderView.swift
+//  tomaListaApp
+//
+//  Created by PEDRO MUNOZ BURGOS on 6/23/23.
+//
+
+import UIKit
+
+class AuthHeaderView: UIView {
+    
+    // MARK: - Variables
+    
+    //
+
+
+
+}
